2ec279fb0f7ad92dceae14ee5496921b44634050
InsigMath <cris.kovacs14@gmail.com>
2020-05-02 12:29:54 -0400

    Add files via upload

---
+++ MainInstances.gd
@@ -0,0 +1,5 @@
+extends Resource
+class_name MainInstances
+
+var Player = null # preload("res://Metroidvania Resources/Player/PlayerStats.tres")
+var WorldCamera = null
---
+++ ResourceLoader.gd
@@ -0,0 +1,5 @@
+extends Node
+
+# warning-ignore-all:unused_class_variable
+var PlayerStats = preload("res://Metroidvania Resources/Player/PlayerStats.tres")
+var MainInstances = preload("res://MainInstances.tres")
